280fe1996e868e7c6f2ae77740068a539ccff6ef
LeeMinglu <402007353@qq.com>
2016-01-01 20:35:56 +0800

    GCD线程间通讯

--- GCD_FINAL/GCD_FINAL/ViewController.m
+++ GCD_FINAL/GCD_FINAL/ViewController.m
@@ -16,10 +16,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self demoGCD1];
+//    [self demoGCD1];
+//    [self demoGCD2];
+    
+//    [self demoGCD3];
     
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    [self demoGCD3];
+    
+}
+
+//线程间通讯
+- (void)demoGCD3 {
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        NSLog(@"玩命加载中...%@", [NSThread currentThread]);
+        
+       //更新UI
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            NSLog(@"更新UI...%@", [NSThread currentThread]);
+        });
+    });
+}
+
+//精简版
+- (void)demoGCD2 {
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        NSLog(@"%@", [NSThread currentThread]);
+    });
+    
+}
 
 
 - (void)demoGCD1 {
